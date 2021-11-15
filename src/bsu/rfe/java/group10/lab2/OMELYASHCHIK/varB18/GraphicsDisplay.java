ba16b76057ea5e9976d90029181560e992f19ccd
Ivan Omelyashchik <cpt.legion.337@gmail.com>
2021-11-15 13:31:50 +0300

    First commit

---
+++ src/bsu/rfe/java/group10/lab2/OMELYASHCHIK/varB18/GraphicsDisplay.java
@@ -0,0 +1,41 @@
+package bsu.rfe.java.group10.lab2.OMELYASHCHIK.varB18;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphicsDisplay extends JPanel {
+    // Список координат точек для построения графика
+    private Double[][] graphicsData;
+    // Флаговые переменные, задающие правила отображения графика
+    private boolean showAxis = true;
+    private boolean showMarkers = true;
+    // Границы диапазона пространства, подлежащего отображению
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+    // Используемый масштаб отображения
+    private double scale;
+    // Различные стили черчения линий
+    private BasicStroke graphicsStroke;
+    private BasicStroke axisStroke;
+    private BasicStroke markerStroke;
+    // Шрифт отображения подписей к осям координат
+    private Font axisFont;
+
+    public GraphicsDisplay() {
+        setBackground(Color.WHITE);
+        graphicsStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_ROUND, 10.0f, null, 0.0f);
+        // Перо для рисования осей координат
+        axisStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+// Перо для рисования контуров маркеров
+        markerStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+// Шрифт для подписей осей координат
+        axisFont = new Font("Serif", Font.BOLD, 36);
+    }
+
+}
+
---
+++ src/bsu/rfe/java/group10/lab2/OMELYASHCHIK/varB18/MainApplication.java
@@ -0,0 +1,7 @@
+package bsu.rfe.java.group10.lab2.OMELYASHCHIK.varB18;
+
+public class MainApplication {
+    public static void main (String[] args){
+
+    }
+}
